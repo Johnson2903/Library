49c008cfd2099e79db7b02abf1b64c388b05dc97
johnson2903 <johnsonadelere.29@gmail.com>
2019-10-30 05:14:45 -0700

    my library project. first commit

---
+++ lib.js
@@ -0,0 +1,45 @@
+
+function myAdd(){
+	if (firstone.value || secondone.value || thirdone.value=="" ) {
+alert("THe space must not be empty");
+console.log("THe place must not be empty");
+	}
+
+
+	else{
+	var myarr=JSON.parse(localStorage.NewLib);
+	var Myobj={
+		BookName :firstone.value,
+		AuthorName:secondone.value,
+		PagesofBook: thirdone.value
+	}
+
+	myarr.push(Myobj)
+	var m=localStorage.NewLib=JSON.stringify(myarr)
+	JSON.parse(localStorage.NewLib);}
+	
+}
+
+function myview(){
+	u=JSON.parse(localStorage.NewLib);
+	var res = '';
+	for (var i = 0; i< u.length; i++) {
+		var y="<td>" + u[i].BookName+"</td>" +"<td>"+ u[i].AuthorName+"</td>" +
+		 "<td>"+ u[i].PagesofBook+"</td>"
+		var at="<tr>" +y+"</tr>"
+ 
+	res+=at;
+
+	}
+	tt.innerHTML = res;
+	
+
+	console.log(u)
+
+}
+
+function myclean(){
+	firstone.value=""
+	secondone.value=""
+	thirdone.value=""
+};
